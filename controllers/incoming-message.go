ffe008ff28505123c4c62e60c285401808be7a52
Mateusz Koroś <mateusz.koros@protonmail.com>
2018-09-04 19:00:25 +0200

    Added option to upload files

--- controllers/incoming-message.go
+++ controllers/incoming-message.go
@@ -2,9 +2,13 @@ package controllers
 
 import (
 	"BeeMail/database"
+	"BeeMail/helpers"
 	"BeeMail/models"
-	"encoding/json"
 	"github.com/astaxie/beego"
+	"io"
+	"os"
+	"regexp"
+	"strings"
 )
 
 type IncomingMessageController struct {
@@ -13,7 +17,31 @@ type IncomingMessageController struct {
 
 func (c *IncomingMessageController) Post() {
 	var msg models.Message
-	json.Unmarshal(c.Ctx.Input.RequestBody, &msg)
+	var subject, message string
+	c.Ctx.Request.ParseMultipartForm(32 << 20)
+	file, handler, _ := c.Ctx.Request.FormFile("Attachment")
+	if len(c.Ctx.Request.Form["Subject"]) > 0 {
+		subject = c.Ctx.Request.Form["Subject"][0]
+	}
+	if len(c.Ctx.Request.Form["Message"]) > 0 {
+		message = c.Ctx.Request.Form["Message"][0]
+	}
+	if strings.TrimSpace(subject) != "" {
+		msg.Subject = subject
+	}
+	if strings.TrimSpace(message) != "" {
+		msg.Message = message
+	}
+	if file != nil {
+		defer file.Close()
+		filename := validateFileName(handler.Filename)
+		f, err := os.Create("./" + filename)
+		if err != nil {
+			beego.Warn(err)
+		}
+		defer f.Close()
+		io.Copy(f, file)
+	}
 	if msg.IsEmpty() {
 		c.Data["json"] = map[string]string{"Response": "Message provided in improper format"}
 	} else {
@@ -23,4 +51,13 @@ func (c *IncomingMessageController) Post() {
 	}
 	beego.Info("Received message:\n" + string(c.Ctx.Input.RequestBody))
 	c.ServeJSON()
+}
+
+// remove potentially harmful characters from filename
+func validateFileName(fileName string) string {
+	validator, err := regexp.Compile(`[*\\/"\[\]:;|=,&]`)
+	if err != nil {
+		helpers.CheckError(err)
+	}
+	return validator.ReplaceAllString(fileName, "")
 }
