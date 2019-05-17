7e8d7b08365761cb2b24ee9d62fae103bff80d08
Mateusz Koroś <mateusz.koros@protonmail.com>
2019-05-17 17:55:08 +0200

    Added test for checking addresses after inserted message

--- tests/controllers_test.go
+++ tests/controllers_test.go
@@ -1,7 +1,10 @@
 package test
 
 import (
+	"BeeMail/database"
 	"BeeMail/helpers"
+	"BeeMail/models"
+	"fmt"
 	"github.com/astaxie/beego"
 	. "github.com/smartystreets/goconvey/convey"
 	"net/http"
@@ -49,6 +52,31 @@ func TestGetEmptyMessages(t *testing.T) {
 		})
 		Convey("The Result Should Be Empty", func() {
 			So(w.Body.String(), ShouldEqual, "[]")
+		})
+	})
+}
+
+func TestGetAddressesAfterMessageReceived(t *testing.T) {
+	cleanUp()
+	addressToInsert := "10.10.10.10"
+	mail := models.Mail{Type: models.Incoming, RemoteAddress: addressToInsert}
+	db := *(database.GetInstance())
+	_, err := db.Insert(&mail)
+	r, _ := http.NewRequest("GET", "/addresses", nil)
+	w := httptest.NewRecorder()
+	beego.BeeApp.Handlers.ServeHTTP(w, r)
+
+	beego.Trace("testing", "TestBeego", "Code[%d]\n%s", w.Code, w.Body.String())
+
+	Convey("Subject: Test Station Endpoint\n", t, func() {
+		Convey("Database insertion should be successful", func() {
+			So(err, ShouldBeNil)
+		})
+		Convey("Status Code Should Be 200", func() {
+			So(w.Code, ShouldEqual, 200)
+		})
+		Convey("The Result Should Not Be Empty", func() {
+			So(w.Body.String(), ShouldEqual, fmt.Sprintf("[\"%s\"]", addressToInsert))
 		})
 	})
 }
