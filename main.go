fca322d69537e6532a43ad243a0b1e64dca4b7d4
Mateusz Koroś <mateusz.koros@protonmail.com>
2019-05-22 22:44:37 +0200

    Moved configuration to code to get rid of external dependencies during deployment

--- main.go
+++ main.go
@@ -7,10 +7,24 @@ import (
 	"github.com/astaxie/beego"
 )
 
+func setup() {
+	beego.BConfig.AppName = "BeeMail"
+	beego.BConfig.ServerName = "BeeMail"
+	beego.BConfig.Listen.EnableHTTP = false
+	beego.BConfig.Listen.EnableHTTPS = true
+	beego.BConfig.Listen.HTTPSPort = 1944
+	beego.BConfig.Listen.HTTPSCertFile = "cryptography/BeeMail.crt"
+	beego.BConfig.Listen.HTTPSKeyFile = "cryptography/BeeMail.key"
+	beego.BConfig.RunMode = "prod"
+	beego.BConfig.CopyRequestBody = true
+}
+
 func init() {
 	helpers.CreateCertificateIfNotExists()
 	database.GetInstance()
 }
+
 func main() {
+	setup()
 	beego.Run()
 }
